49abd5d8273789e4e2e74d93a83b25ee9606b3e7
AndreiOlegovich <info@eth1.ru>
2024-02-20 16:02:24 +0200

    Adds tickets and starts using f-sting

--- demo_package/demo_module.py
+++ demo_package/demo_module.py
@@ -1,6 +1,9 @@
 def demo_function(service: str) -> str:
-    service_dict = {"hosting": "https://beget.com"}
+    service_dict = {
+            "hosting": "https://beget.com"
+            "tickets": "https://aviasales.com"
+    }
     if service in service_dict:
-        return service_dict[service]
+        return f"Recommended service for {service} is {service_dict[service]}"
     else:
         return "Unknown service"
