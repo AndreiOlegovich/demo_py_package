bc191468625eb4c3dbfc2e4c03fe126c9f191cef
AndreiOlegovich <info@eth1.ru>
2024-02-20 12:49:11 +0200

    Initial commit

---
+++ demo/demo/demo_url.py
@@ -0,0 +1,6 @@
+def url_gen(service: str) -> str:
+    service_dict = {"hosting": "https://beget.com"}
+    if service in service_dict:
+        return service_dict(service)
+    else
+        return "Unknown service"
