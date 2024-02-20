4e662fcba1f245e46077ec9c2dba68b0d3c39822
AndreiOlegovich <info@eth1.ru>
2024-02-20 13:00:06 +0200

    Typo fix

--- demo/demo_url.py
+++ demo/demo_url.py
@@ -2,5 +2,5 @@ def url_gen(service: str) -> str:
     service_dict = {"hosting": "https://beget.com"}
     if service in service_dict:
         return service_dict(service)
-    else
+    else:
         return "Unknown service"
